6d5058683a368aeb48381dfb0e7c27b4ad2e3c6a
Francisco Valdez <isc.valdezfrancisco@gmail.com>
2017-07-18 00:27:47 -0700

    Add functions for belts and sensors
    control for the belts and the sensors

--- arduino_belt.ino
+++ arduino_belt.ino
@@ -1,17 +1,56 @@
+
+#include <Servo.h>
 #include <ContinuousRotationServo.h>
 
-ContinuousRotationServo Servo;
+// bandas
+ContinuousRotationServo banda_izq;
+ContinuousRotationServo banda_der;
 int distance;
 
+
+// bandas
+int servo_ban_der = 6;
+int servo_ban_izq = 7; 
+Servo servo_der;
+Servo servo_izq;  
+int servoAngle = 0;   // servo position in degrees
+
+//palancas
+int servo_pal_der = 2;
+int servo_pal_izq = 3;
+Servo pal_izq;
+Servo pal_der;
+ 
 void setup()
 {
-  Servo.begin(8);
-}
+  Serial.begin(9600);  
+  servo_der.attach(servo_ban_der);
+  servo_izq.attach(servo_ban_izq); 
 
+  
+
+}
+ 
 void loop()
 {
-  Servo.rotateLeft(50,1);
-  //Servo.noMovement(500);
-  //Servo.rotateRight(50,100);
-  //Servo.noMovement(500);
+  //banda_izq.rotateRight(90,100);
+  servo_der.write(45);      // Turn SG90 servo Left to 45 degrees
+  servo_izq.write(45);      // Turn SG90 servo Left to 45 degrees  
 }
+ 
+void girar_pal_der()
+{
+   pal_der.write(0);      // Turn SG90 servo Left to 45 degrees
+   delay(1000);          // Wait 1 second
+   pal_der.write(180);      // Turn SG90 servo back to 90 degrees (center position)
+   delay(1000);          // Wait 1 second
+}
+
+void girar_pal_izq()
+{
+   pal_izq.write(0);      // Turn SG90 servo Left to 45 degrees
+   delay(1000);          // Wait 1 second
+   pal_izq.write(180);      // Turn SG90 servo back to 90 degrees (center position)
+   delay(1000);          // Wait 1 second
+}
+
